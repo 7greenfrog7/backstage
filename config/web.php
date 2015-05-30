eb2ab6ac83d94c7e816f219ece366100d570710e
zjj <1765559690@qq.com>
2015-05-30 15:33:20 +0800

    to generate a private code to run web:zjjpqp7788

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'zjjpqp7788',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
